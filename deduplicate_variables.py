13033afab71ab9d722559ba466d4c0bcb9881b38
Quinn Tucker <qxzcraft@gmail.com>
2025-05-15 20:36:52 -0400

    Only deduplicate used variables

--- deduplicate_variables.py
+++ deduplicate_variables.py
@@ -6,11 +6,18 @@ from disassembly_types import LinearExpr, Variable
 with open("generated_data/variables.pickle", "rb") as f:
     variables: list[Variable] = pickle.load(f)
 
+with open("generated_data/used_variables.json") as f:
+    is_var_used: list[bool] = json.load(f)
+
 last_num_unique = -1
 while True:
-    num_unique = len(set(var.value for var in variables if var.value is not None))
+    values = [
+        var.value for i, var in enumerate(variables) if is_var_used[i] and var.value is not None
+    ]
+    num_unique = len(set(values))
     print(
-        f"{num_unique} unique values ({100 * num_unique / len(variables):.2f}% of {len(variables)})"
+        f"{num_unique} unique values "
+        f"({100 * num_unique / len(values):.2f}% of {len(values)} used variables with values)"
     )
     if num_unique == last_num_unique:
         break
@@ -18,15 +25,17 @@ while True:
 
     remap: dict[LinearExpr, int] = {}
     for i, var in enumerate(variables):
-        if var.value is not None and var.value not in remap:
+        if is_var_used[i] and var.value is not None and var.value not in remap:
             remap[var.value] = i
 
-    remap_var_index = [
-        i if var.value is None else remap[var.value] for i, var in enumerate(variables)
-    ]
+    remap_var_index = {
+        i: i if var.value is None else remap[var.value]
+        for i, var in enumerate(variables)
+        if is_var_used[i]
+    }
 
-    for var in variables:
-        if var.value is not None:
+    for i, var in enumerate(variables):
+        if is_var_used[i] and var.value is not None:
             var.value = LinearExpr(
                 constant=var.value.constant,
                 terms=frozenset((remap_var_index[i], coef) for i, coef in var.value.terms),
